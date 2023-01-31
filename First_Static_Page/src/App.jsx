6f38e7ee4fd662983aaeb5938c1d3ed033eae6da
Sankalp Gunturi <sgunturi@cmu.edu>
2023-01-31 08:06:23 -0800

    Added First Static Page using React

---
+++ First_Static_Page/src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
