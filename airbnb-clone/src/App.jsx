f54dc578494ce70dc6f57e9f3730feab785db95e
Sankalp Gunturi <sgunturi@cmu.edu>
2023-01-31 17:59:00 -0800

    Fully functional static page

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
 
+  // <Hero />
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
     </div>
-  )
+  );
 }
-
-export default App
---
+++ airbnb-clone/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return(
+        <nav className='nav'>
+            <img src="../src/assets/airbnb-logo.png" alt="airbnb-logo" className="nav--logo" />
+        </nav>
+    )
+}
