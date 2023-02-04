b1b0cee3a2c235ca5a229a9e97aed5e3e69f9950
Sankalp Gunturi <sgunturi@cmu.edu>
2023-02-04 10:37:50 -0800

    More react hooks

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -8,6 +8,8 @@ import { Oscar } from "./components/Oscar";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import { Container } from "./components/Container";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { Box } from "./components/context/Box";
 
 function App() {
   const personName = {
@@ -51,6 +53,9 @@ function App() {
         }}
       />
       <Container styles={{ border: "1px solid black", padding: "1rem" }} />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
--- react-typescript-demo/src/components/state/User.tsx
+++ react-typescript-demo/src/components/state/User.tsx
@@ -6,20 +6,21 @@ type AuthUser = {
 };
 
 export const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  // const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({ name: "Sankalp", email: "sankalp@cc.com" });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+  // const handleLogout = () => {
+  //   setUser(null);
+  // };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>Username is {user?.name}</div>
-      <div>Email is {user?.email}</div>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      <div>Username is {user.name}</div>
+      <div>Email is {user.email}</div>
     </div>
   );
 };
